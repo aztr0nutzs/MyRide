461fb4003367a2eb1b1d90490ff2d6ef575ed6dc
Thomas Washburn <1up5dwwn@gmail.com>
2026-08-19 09:47:50 -0400

    Phase 2 bootstrap: add Android application module

---
+++ app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.myride.app"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.myride.app"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
