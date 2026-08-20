d89fff4a1e3358fd60b314770736b328c9290359
Thomas Washburn <1up5dwwn@gmail.com>
2026-08-19 22:03:54 -0400

    Expand MyRide schema to persistence target version 2

--- app/src/main/java/com/myride/app/persistence/MyRideDatabase.kt
+++ app/src/main/java/com/myride/app/persistence/MyRideDatabase.kt
@@ -3,15 +3,35 @@ package com.myride.app.persistence
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.myride.app.persistence.dao.VehicleDao
-import com.myride.app.persistence.entity.VehicleEntity
+import com.myride.app.persistence.dao.*
+import com.myride.app.persistence.entity.*
 
 @Database(
-    entities = [VehicleEntity::class],
-    version = 1,
+    entities = [
+        VehicleEntity::class,
+        MaintenanceEventEntity::class,
+        MaintenanceTaskEntity::class,
+        InspectionDefinitionEntity::class,
+        InspectionSessionEntity::class,
+        DiagnosticSessionEntity::class,
+        ProcedureEntity::class,
+        KnowledgeEntryEntity::class,
+        AttachmentEntity::class,
+        AppSettingEntity::class
+    ],
+    version = 2,
     exportSchema = true
 )
 @TypeConverters(Converters::class)
 abstract class MyRideDatabase : RoomDatabase() {
     abstract fun vehicleDao(): VehicleDao
+    abstract fun maintenanceEventDao(): MaintenanceEventDao
+    abstract fun maintenanceTaskDao(): MaintenanceTaskDao
+    abstract fun inspectionDefinitionDao(): InspectionDefinitionDao
+    abstract fun inspectionSessionDao(): InspectionSessionDao
+    abstract fun diagnosticSessionDao(): DiagnosticSessionDao
+    abstract fun procedureDao(): ProcedureDao
+    abstract fun knowledgeEntryDao(): KnowledgeEntryDao
+    abstract fun attachmentDao(): AttachmentDao
+    abstract fun appSettingDao(): AppSettingDao
 }
