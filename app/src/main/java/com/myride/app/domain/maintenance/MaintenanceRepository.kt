9ca8b21c8278f5b6b2ad78652c3ca18999557863
Thomas Washburn <1up5dwwn@gmail.com>
2026-08-19 20:05:00 -0400

    Make maintenance repository contract asynchronous

--- app/src/main/java/com/myride/app/domain/maintenance/MaintenanceRepository.kt
+++ app/src/main/java/com/myride/app/domain/maintenance/MaintenanceRepository.kt
@@ -1,7 +1,7 @@
 package com.myride.app.domain.maintenance
 
 interface MaintenanceRepository {
-    fun get(eventId: String): MaintenanceEvent?
-    fun save(event: MaintenanceEvent)
-    fun delete(eventId: String)
+    suspend fun get(eventId: String): MaintenanceEvent?
+    suspend fun save(event: MaintenanceEvent)
+    suspend fun delete(eventId: String)
 }
